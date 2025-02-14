aa6fdbd738ac2abf892913def5bf595f4f05050e
Ibnodam <144240270+Ibnodam@users.noreply.github.com>
2025-02-14 16:44:53 +0200

    Add files via upload

---
+++ Lab2-BinaryTrees/Lab2-BinaryTrees/Program.cs
@@ -0,0 +1,14 @@
+﻿using Lab2_BinaryTrees;
+
+
+    TreeNode root = new TreeNode(1);
+    root.Left = new TreeNode(2);
+    root.Right = new TreeNode(3);
+    root.Left.Left = new TreeNode(4);
+    root.Left.Right = new TreeNode(5);
+    root.Right.Right = new TreeNode(6);
+
+    Tree tree = new Tree(root);
+
+    int sum = tree.SumOfLeaves(tree.Root);
+    Console.WriteLine($"Сумма значений всех листьев: {sum}");
---
+++ Lab2-BinaryTrees/Lab2-BinaryTrees/Tree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_BinaryTrees
+{
+    public class Tree
+    {
+        public TreeNode Root;
+
+        public Tree(TreeNode root)
+        {
+            Root = root;
+        }
+        public int SumOfLeaves(TreeNode node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            if (node.Left == null && node.Right == null)
+            {
+                return node.Value;
+            }
+
+            return SumOfLeaves(node.Left) + SumOfLeaves(node.Right);
+        }
+    }
+
+}
